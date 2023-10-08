8c1e7ab27e381fcef6fe7aa770af756313d21aee
Tali Marouf <tali.marouf@outlook.fr>
2023-10-08 21:13:22 +0200

    intégration footer

---
+++ wp-content/themes/Planty/footer.php
@@ -0,0 +1,11 @@
+<footer id="site-footer" clcass="header-footer-group" >
+    <div class="footer-content">
+        <a href="#">Mentions légales</a>
+    </div>
+</footer>
+
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -4,4 +4,6 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 { //on s assure que l on charge bien les style du theme parent
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+  //et notre style theme.css
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
