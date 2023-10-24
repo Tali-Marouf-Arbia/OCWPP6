8749e85f53e2e89e429dd8a1c1cd38781e69d25f
Tali Marouf <tali.marouf@outlook.fr>
2023-10-24 15:14:28 +0200

    intégration du hook

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -10,37 +10,19 @@ function theme_enqueue_styles()
 
 // add_filter('wp_nav_menu_items','add_admin_link', 10, 2);
 // function add_admin_link($items, $args) {
-//   if (is_user_logged_in() && $args->theme_location == 'nav-menus') {
+//   if (is_user_logged_in() && $args->theme_location == 'primary') {
 //     $items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';
 //   }
 //   return $items;
 // }
 
-// add_filter('wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2);
+add_filter('wp_nav_menu_items','add_admin_link', 10, 2);
+function add_admin_link($items, $args) {
+  if (is_user_logged_in() && $args->theme_location == 'primary') {
+    $admin_link = '<li><a href="'. get_admin_url() .'">Admin</a></li>';
+    $items = $admin_link . $items;
+  }
+  return $items;
+}
 
-// function ajouter_lien_admin_au_menu($items, $args) {
-//   if (is_user_logged_in() && current_user_can('administrator') && $args->theme_location == 'primary-menu') {
-//     $admin_url = admin_url();
-//     $items .= '<li><a href="' . esc_url($admin_url) . '">Admin</a></li>';
-//   }
-//   return $items;
-// }
-
-// function add_admin_link_to_menu($items, $args) {
-//   if (is_user_logged_in() && current_user_can('manage_options')) {
-//       // Créez un élément de menu pour le lien "Admin"
-//       $admin_link = '<li id="menu-item-admin" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-admin"><a href="' . admin_url() . '">Admin</a></li>';
-      
-//       // Trouvez l'ID de l'élément après lequel vous souhaitez insérer "Admin"
-//       $insert_after = 'menu-item-43'; // Remplacez 'menu-item-38' par l'ID de l'élément après lequel vous souhaitez insérer "Admin"
-
-//       // Insérez l'élément "Admin" après l'élément spécifié
-//       $index = array_search($insert_after, array_column($items, 'ID'));
-//       if ($index !== false) {
-//           array_splice($items, $index + 1, 0, array("admin_link" => $admin_link));
-//       }
-//   }
-//   return $items;
-// }
-// add_filter('wp_nav_menu_objects', 'add_admin_link_to_menu', 10, 2);
 
