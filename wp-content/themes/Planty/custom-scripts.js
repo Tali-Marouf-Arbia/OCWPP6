8e9a3d7784329e0994d1e3821ac27ee08dbd0f95
Tali Marouf <tali.marouf@outlook.fr>
2023-11-07 11:12:46 +0100

    Optimisation pour les petis écrans et nettoyage css

--- wp-content/themes/Planty/custom-scripts.js
+++ wp-content/themes/Planty/custom-scripts.js
@@ -1,11 +1,17 @@
-// Sélectionne tous les boutons "plus" et "moins"
-var btnPlus = document.querySelectorAll('.btn-plus');
-var btnMoins = document.querySelectorAll('.btn-moins');
+var btnPlus = document.querySelectorAll('.btn-plus');  
+var btnMoins = document.querySelectorAll('.btn-moins');  
 
-// Ajoute des gestionnaires d'événements pour les boutons "plus"
+
 btnPlus.forEach(function(button) {
-    button.addEventListener('click', function() {
+    button.addEventListener('click' , function() {
         var inputField = this.closest('.input-groupe').querySelector('.input-form-commande');
         inputField.value = parseInt(inputField.value) + 1;
+    });
+});
+
+btnMoins.forEach(function(button) {
+    button.addEventListener('click' , function() {
+        var inputField = this.closest('.input-groupe').querySelector('.input-form-commande');
+        inputField.value = parseInt(inputField.value) - 1;
     });
 });
